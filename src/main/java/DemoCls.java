384dad29f27c43f7a376d37d70752fefd711f6c5
Sharath <sharathkumar502@gmail.com>
2015-06-10 11:36:29 -0500

    Dirty Code

--- src/main/java/DemoCls.java
+++ src/main/java/DemoCls.java
@@ -3,6 +3,7 @@ public class DemoCls {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final String name;
 		System.out.println("Hello World");
 		System.out.println("Build Maven Project Successfully");
 		System.out.println("Scheduled Jenkins for auto build");
